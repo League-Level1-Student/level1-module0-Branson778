30175100881857069376881d5abb0f74b70b24cc
student <student@ip-172-31-51-185.us-west-2.compute.internal>
2020-10-31 17:31:19 -0700

    Houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,37 @@
 package _01_houses;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	static Robot rob = new Robot();
 	public void run() {
 		
+		rob.setX(51);
+		rob.setY(494);
+		rob.penDown();
+	int height = 100;
+	int length = 50;
+	buildHouse();
 	}
+	static int height = 100;
+	static int length = 50;
+	public static void buildHouse() {
+		rob.setPenColor(Color.BLACK);
+		rob.turn(90);
+		rob.move(height);
+		rob.turn(90);
+		rob.move(length);
+		rob.turn(90);
+		rob.move(height);
+		rob.move(90);
+		rob.setPenColor(Color.GREEN);
+		rob.move(length);
+		rob.setPenColor(Color.BLACK);
+	}
+	
+	
+	
+	
 }
